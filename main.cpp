5fc9723337987360e3660c77b7568e77d209d347
Metreveli-A <116972081+Metreveli-A@users.noreply.github.com>
2022-11-19 12:12:51 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numb1;
+    int numb2;
+    bool result = true;
+    
+    cout <<"enter first number: ";
+    cin>>numb1;
+
+    cout <<"enter second number: ";
+    cin>>numb2;
+    
+if(numb1>numb2){
+    
+    cout<<numb1;
+}else{
+    cout<<numb2;
+}
+    
+   
+
+    return 0;
+
+}
